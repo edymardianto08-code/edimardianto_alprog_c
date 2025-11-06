4d3a3a849dd1dcf4cc1ca01599d532ea921f4388
EDI MARDIANTO_Alprog-C <edymardianto08@gmail.com>
2025-11-06 14:34:32 +0800

    Add files via upload

---
+++ boby.py
@@ -0,0 +1,8 @@
+print("edi mardianto")
+print("nim 25241096")
+print("Saya harapan diri sendiri")
+print("Tulang punggung keluarga")
+
+print("MELANGKAH DENGAN BISMILLAH")
+print("PULANG DAN SUKSES DENGAN ALHAMDULILLAH")
+
---
+++ bobyUntitled-2.py
@@ -0,0 +1,18 @@
+#variabel = cara kita menyimpan nilai/value(aljabar)
+
+# memberikan nilai (assisment)
+x = 13
+y = 20
+lebar = 30
+
+print("angka x adalah : ", x)
+print("angka y adalah : ", y)
+print("angka lebar adalah : ", lebar)
+
+# aturan penamaan variabel
+bat99 = 100 # tidak boleh diawali angka
+bobyYanto = 100 # juka lebiih dr 1 kata gabung dengan _
+jumlah hutang = 100000 # bedakan dengan kapital
+
+#pemanggilan inderet
+z = x
