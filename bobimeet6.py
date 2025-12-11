778c8707e858879d90020fed9fb729de58462705
EDI MARDIANTO_Alprog-C <edymardianto08@gmail.com>
2025-12-11 12:50:00 +0800

    Add files via upload

---
+++ bobimeet6.py
@@ -0,0 +1,16 @@
+nama: edi mardianto
+nim : 25241096
+
+#suhu saya dalam celcius
+celcius = 30
+
+# konversi suhu 
+fahranhit = (9/5)* celcius + 32
+reamur = (4/5)* celcius
+kelvin = celcius + 273.15
+
+#uotput
+print("suhu saya dalam celcius :",celcius)
+print("suhu saya dalam fahranhit :",fahranhit)
+print("suhu saya dalam reamur :",reamur)
+print("suhu saya dalam kelvin :",kelvin)
